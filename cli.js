40ef3fe7bcde1ae7ebd1cc59fb1b01eb0255acd9
alex.storkey <alex.storkey@eckoh.com>
2014-08-28 13:44:40 +0100

    fixing password?

--- cli.js
+++ cli.js
@@ -65,7 +65,7 @@ var p = rankings.then(function (rankings) {
     concurrency: 5
 });
 
-var saveHero = db('postgres://d3i:B644&I"\$4j},eU@localhost/');
+var saveHero = db('postgres://d3i:eeee@localhost/');
 p = p.each(function (hero) {
     return saveHero(hero);
 });
