6dea97a539453a2441e65b300a1f21da37ba497e
stork <stork@not.gd>
2014-09-01 20:30:05 +0100

    ugh what

--- queries.sql
+++ queries.sql
@@ -7,13 +7,12 @@ select hero.class, sk.name as skill, count(*) as count
     group by skill, hero.class
     order by hero.class, count desc;
 
-select hero.class, sk.name as skill, count(*) as count, avg(hero.ranking_tier) as avgRank,
-            avg(hero.paragon_level) as avgParagon
+select hero.class, sk.name as skill, count(*) as count
         from hero_skills hs
-        join hero hero on hero.id = hs.hero_id
-        join skills sk on sk.id = hs.skill_id and sk.type = 'passive' and sk.class = hero.class
+        inner join hero hero on hero.id = hs.hero_id
+        inner join skills sk on sk.id = hs.skill_id and sk.type = 'passive' and sk.class = hero.class
         WHERE hero.class='barbarian' and not hero.hardcore
-        group by skill, hero.class
+        group by sk.name, hero.class
         order by hero.class, count desc;
 
 select hero.class
@@ -27,3 +26,14 @@ SELECT class, hardcore, host, count(*), min(ranking_tier) min_tier, max(ranking_
         avg(last_updated) as age
     FROM hero
     GROUP BY class, hardcore, host
+
+
+
+
+
+select sk.name as skill, count(*) as count
+        from hero_skills hs
+        inner join hero hero on hero.id = hs.hero_id
+        inner join skills sk on sk.id = hs.skill_id and sk.type = 'passive' and sk.class = hero.class
+        group by sk.name
+        order by count desc;
