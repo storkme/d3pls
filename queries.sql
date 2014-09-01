caeb021444dd555af720554c0167c55c2edf883b
alex.storkey <alex.storkey@eckoh.com>
2014-09-01 15:51:34 +0100

    more concurrency, hopefully less fail?

--- queries.sql
+++ queries.sql
@@ -1,19 +1,20 @@
--- popular active skills + runesw2gy#jm1111uij
-select hero.class, sk.name as skill, ru.name as rune, count(*) as count
-    from d3i.hero_skills hs
-        join d3i.hero hero on hero.id = hs.hero_id
-        join d3i.skills sk on sk.id = hs.skill_id and sk.type = 'skill' and sk.class = hero.class
-        join d3i.skills ru on ru.id = hs.rune_id and ru.parent_id = hs.skill_id and ru.type = 'rune' and ru.class = hero.class
-    group by skill, rune, hero.class
-    order by hero.class, count desc;
-
 -- popular passives
 select hero.class, sk.name as skill, count(*) as count
-    from d3i.hero_skills hs
-        join d3i.skills sk on sk.id = hs.skill_id and sk.type = 'passive' and hs.rune_id is null
-        join d3i.hero hero on hero.id = hs.hero_id
+    from hero_skills hs
+        join skills sk on sk.id = hs.skill_id and sk.type = 'passive' and hs.rune_id is null
+        join hero hero on hero.id = hs.hero_id
+    where not hero.hardcore
     group by skill, hero.class
     order by hero.class, count desc;
+
+select hero.class, sk.name as skill, count(*) as count, avg(hero.ranking_tier) as avgRank,
+            avg(hero.paragon_level) as avgParagon
+        from hero_skills hs
+        join hero hero on hero.id = hs.hero_id
+        join skills sk on sk.id = hs.skill_id and sk.type = 'passive' and sk.class = hero.class
+        WHERE hero.class='barbarian' and not hero.hardcore
+        group by skill, hero.class
+        order by hero.class, count desc;
 
 select hero.class
     from d3i.hero
