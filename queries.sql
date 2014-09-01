3b34bc5ec44177af294af3b540a14416e1d42636
stork <stork@not.gd>
2014-09-01 21:39:44 +0100

    what a confusing postgres i don't even

--- queries.sql
+++ queries.sql
@@ -11,7 +11,7 @@ select hero.class, sk.name as skill, count(*) as count
         from hero_skills hs
         inner join hero hero on hero.id = hs.hero_id
         inner join skills sk on sk.id = hs.skill_id and sk.type = 'passive' and sk.class = hero.class
-        WHERE hero.class='barbarian' and not hero.hardcore
+        WHERE hero.class='barbarian' and hero.hardcore = 't'
         group by sk.name, hero.class
         order by hero.class, count desc;
 
@@ -27,13 +27,28 @@ SELECT class, hardcore, host, count(*), min(ranking_tier) min_tier, max(ranking_
     FROM hero
     GROUP BY class, hardcore, host
 
+select hero.*, skills.name from hero_skills
+        inner join hero on hero.id = hero_skills.hero_id
+        inner join skills on (skills.id = hero_skills.skill_id and skills.type = 'passive')
+        where hero.class='barbarian';
 
 
 
-
-select sk.name as skill, count(*) as count
-        from hero_skills hs
-        inner join hero hero on hero.id = hs.hero_id
-        inner join skills sk on sk.id = hs.skill_id and sk.type = 'passive' and sk.class = hero.class
-        group by sk.name
-        order by count desc;
+select
+    hero.*,
+    hero_skills.*,
+    skills.name,
+    skills.id,
+    skills.type
+from hero
+join hero_skills
+    on hero_skills.hero_id = hero.id
+join skills
+    on skills.id = hero_skills.skill_id
+        and skills.class = hero.class
+        and hero_skills.rune_id is null
+where hero.class = 'barbarian'
+and skills.type = 'passive'
+order by
+    hero.id asc,
+    skills.name asc;
